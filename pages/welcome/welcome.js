7063ca457bf5dfb4c01f0dfff9b9206dcc33d5df
TuoLinHuai <TuoLinHuai@123.com>
2020-03-31 23:00:26 +0800

    bind  data
    绑定数据，页面跳转

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -3,52 +3,27 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    data: {},
+    //事件处理函数
+    onTap: function () {
+        /*wx.navigateTo({
+            url: '../posts/post'
+        });*/
+
+        wx.redirectTo({
+            url: '../posts/post',
+            success(res) {
+                // 成功才执行该方法
+            },
+            fail(res) {
+                // 失败才执行该方法
+            },
+            complete(res) {
+                // 跳转结束，不管成功与否都执行
+            }
         })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+    },
+    onLoad: function () {
+
+    },
 })
