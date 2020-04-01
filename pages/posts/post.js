599deb9df4038cbbddada7c4fe883e65384b8fdf
TuoLinHuai <TuoLinHuai@123.com>
2020-04-01 23:36:05 +0800

    template
    模板拆分，以及数据导入

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,80 +1,20 @@
 // pages/posts/post.js
+
+var postsData = require('../../data/post-data.js');
 Page({
 
     /**
      * 页面的初始数据
      */
-    data: {
-        posts: [{
-            post_author: "/images/avatar/1.png",
-            post_date: "Nov 25 2016",
-            post_title: "正是虾肥蟹壮时",
-            post_image: "/images/post/crab.png",
-            post_content: "菊黄蟹正肥，品尝秋之味。徐志摩把“看初花的荻芦”和“到楼外楼吃蟹”并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，壳凸红脂块块香”；",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "92",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "65"
-        }, {
-            post_author: "/images/avatar/2.png",
-            post_date: "Nov 25 2016",
-            post_title: "比利·肖恩的故事",
-            post_image: "/images/post/bl.png",
-            post_content: "19岁的德州士兵比利·林恩（乔·阿尔文饰），他在加入美军步兵部队后，被派往伊拉克战场，与其他七名的突击小队成员，在伊拉克与当地反对武装进行了3分43秒的激战，他和战友们在一次交火中大难不死、逃过一劫。",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "160",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "98"
-        }, {
-            post_author: "/images/avatar/2.png",
-            post_date: "Nov 25 2016",
-            post_title: "比利·肖恩的故事",
-            post_image: "/images/post/bl.png",
-            post_content: "19岁的德州士兵比利·林恩（乔·阿尔文饰），他在加入美军步兵部队后，被派往伊拉克战场，与其他七名的突击小队成员，在伊拉克与当地反对武装进行了3分43秒的激战，他和战友们在一次交火中大难不死、逃过一劫。",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "160",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "98"
-        }, {
-            post_author: "/images/avatar/2.png",
-            post_date: "Nov 25 2016",
-            post_title: "比利·肖恩的故事",
-            post_image: "/images/post/bl.png",
-            post_content: "19岁的德州士兵比利·林恩（乔·阿尔文饰），他在加入美军步兵部队后，被派往伊拉克战场，与其他七名的突击小队成员，在伊拉克与当地反对武装进行了3分43秒的激战，他和战友们在一次交火中大难不死、逃过一劫。",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "160",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "98"
-        }, {
-            post_author: "/images/avatar/2.png",
-            post_date: "Nov 25 2016",
-            post_title: "比利·肖恩的故事",
-            post_image: "/images/post/bl.png",
-            post_content: "19岁的德州士兵比利·林恩（乔·阿尔文饰），他在加入美军步兵部队后，被派往伊拉克战场，与其他七名的突击小队成员，在伊拉克与当地反对武装进行了3分43秒的激战，他和战友们在一次交火中大难不死、逃过一劫。",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "160",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "98"
-        }, {
-            post_author: "/images/avatar/2.png",
-            post_date: "Nov 25 2016",
-            post_title: "比利·肖恩的故事",
-            post_image: "/images/post/bl.png",
-            post_content: "19岁的德州士兵比利·林恩（乔·阿尔文饰），他在加入美军步兵部队后，被派往伊拉克战场，与其他七名的突击小队成员，在伊拉克与当地反对武装进行了3分43秒的激战，他和战友们在一次交火中大难不死、逃过一劫。",
-            post_like_image: "../../images/icon/chat.png",
-            post_like_font: "160",
-            post_like_image_view: "../../images/icon/view.png",
-            post_like_font_view: "98"
-        }],
-
-
-    },
+    data: {},
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        this.setData({
+            postList: postsData.postList
+        })
     },
 
     /**
